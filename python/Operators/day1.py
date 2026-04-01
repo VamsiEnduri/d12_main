6aead0dc87b50416fe19ebcb5f9dc4daf2e614c9
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-04-01 12:06:18 +0530

    operators

---
+++ python/Operators/day1.py
@@ -0,0 +1,170 @@
+# # # airthematic 
+# # # + - *
+# # #  / // % **
+# # # /
+
+# # # 10+20
+
+# # # what is operator 
+# # # operator is a symbol which is used to perform some opeartions on operands
+
+# # a=9
+# # b=2
+# # print(a/b) #
+
+
+# # # //
+# # a=9
+# # b=2
+# # print(a//b)
+
+
+# # # # %
+# # a=9
+# # b=2
+# # print(a % b)
+
+
+
+# # a=101
+# # b=3
+# # print(a%b) # 0
+
+# # **
+
+# # a=5
+# # print(a**3)
+
+
+# # compa
+# # > 
+# # a=10
+# # b=100
+# # c=a>b # 10 > 100 #False
+# # print(c) #False
+
+# # >= 
+
+# # a=10
+# # b=100
+# # c=a>=b # 10 > 100 #False
+# # print(c) #False
+
+
+# # d=100
+# # e=100
+# # f=d>=e # 100 >= 100
+
+# # print(f == True) #True :-- 
+
+# # ==
+
+# # print(1 == "1") # 
+# # print(True == "True") # 
+# # print(1 == True) # T
+# # print(0 == False) # T
+# # > 
+# # <=
+# # ==
+# # !=
+# # True == 1
+# # False == 0
+
+# # a=10
+# # b="10"
+# # print(a != b) # 10 != "10" True
+# # print(a != 10) # 10 != 10 False
+
+
+# # print( 1 == True)
+# # print(0 == False)
+# # print(-10 == False)
+# # print(10 == True*10)
+
+
+# # # assignment op
+# # # =
+# # # a = 10
+
+# # # # +=
+# # # a=10 # value assigining
+# # # a=222 # re-assigning
+# # # a="vamsi"  # re-a
+# # cart =0
+# # cart=cart + 1 # 1
+# # cart=cart+10 # 11
+# # print(cart) # 11
+
+# # # # -=
+# # cart=10
+# # cart=cart-2
+# # print(cart)
+# # # *=
+# # a=10
+# # a=a*10
+# # print(a)
+
+# # research topics :-- 
+# # /=
+# # //=
+# # %=
+
+
+# # logical + memebership 
+
+# # logical :-- and, or, not
+
+# # # lo and ro
+# # print(10 == 1 and 10 == 1000) # t and f :-- f
+# # # True and True :-- True
+# #if condition:
+
+
+
+# # and
+# # statements
+# # if 10 == 10 and 20 == 20:
+# #     print("hello")
+# # email ="vamsi@gmail.com"
+# # pswd="12345678"
+
+# # e=input("enter email here :-- ")
+# # p=input("enter paswd here :-- ")
+
+# # if email == e and  pswd == p:
+# #     print("login successful")
+# # else:
+# #     print("login failed")  
+
+# # or
+# # lo or ro :-- either onr of operand true is enough you ill get result as true
+# # print(10 == 100 or 10 == 100) #True
+
+# # not 
+# # print(not not True)
+# # print( not -1)
+# # if -1:
+# #     print("hello")
+# # else:
+# #     print("bye")    
+# # print(10==10 and 20 == 20) # True
+# # print(10==10 and 20 == "vamsi") # False
+# # print(10=="vamsi" and 20 == 20) # False
+# # print(10 == "vamsi" and True == False) # False
+
+
+
+# # membership :-- 
+
+# # print("v" in "vamsi") #true
+# # print("V" in "vamsi") 
+
+# # print(10 in [1,2,3,4,5,6,7,8,10])# True
+# # print(100 in [1,2,3,4,5,6,7,8,10])# False
+
+# # print("name1" in {"id":1,"name":"vamsi"})
+
+# print(10 not in [1,2,3]) #True
+# print("v" not "ravi") # False
+# print("v" not in "akhil") #True
+
---
+++ python/datatypes/mutable_vs_immutable.py
@@ -0,0 +1,109 @@
+# # high - level
+# # interpreted
+# # dymaically typed lang
+# # oops
+# # platform indipendent
+# # programming lang
+
+
+# # data :-- col of info
+# # 2 types :-- p n np
+# # p :-- str, int, float, bool, None
+# # np :-- list, tuple,set,dict
+# # type() :-- used to check type of data
+
+# a=(1,2,3)
+# b=("vamsi",)
+
+# t=type(a)
+# t1=type(b)
+# print(t)
+# print(t1)
+
+# numbers=[1,2,3,4]
+# numbers1=[1]
+
+# print(type(numbers))
+
+
+# details={
+#     "id":1,
+#     "name":"10000coders",
+#     "loc":"hyd",
+#     "pincode":500072
+# }
+# print(type(details))
+
+# abc={}
+# mno={1,2,3,4}
+
+# print(type(abc),"42th line abc variable")
+# print(type(mno),"43rd line")
+
+
+# name="vamsi" # index values
+# print(name[0]) # accessing
+
+# fruits=["banana","apple"] # index values
+# print(fruits[0]) # accessing
+
+# a=10 #assigning 
+# a="20" # re-a
+
+# name=22.5 # index values
+# name[0]="K"
+# print(name)
+
+# fruits=["banana","apple"] # index values
+# print(fruits)
+# fruits[0]="orange" # item  re-assignment
+# print(fruits)
+
+
+# # abc=[11,22,66,"vamsi"]
+# # abc[3]=88
+# # print(abc)
+# # mno="vamsi"
+# # mno[4]="I"
+# # print(mno)
+
+
+
+
+
+
+# details={
+#     "id":1,
+#     "name":"10000coders",
+#     "loc":"hyd",
+#     "pincode":500072
+# }
+
+# print(details["loc"]) # accessing
+# details["loc"]="hyderabad"
+# details["name"]="vamsi"
+# print(details)
+
+
+
+
+# a={1,2,3,4} # keys , index
+# a["first"]="vamsi"
+# print(a)
+
+
+# name="vamsi" # keys , index
+a=(1,2,3,4)
+a[0]=100
+print(a)
+
+
+
+
+
+
+
+
+
+
+
